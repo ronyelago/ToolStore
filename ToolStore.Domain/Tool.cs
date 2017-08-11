49795af5f558a5788c786a0f6f2a82be469fd7be
Ronye <ronye.rocha@gmail.com>
2017-08-10 22:13:35 -0300

    Add project files.

---
+++ ToolStore.Domain/Tool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ToolStore.Domain
+{
+    public class Tool
+    {
+        public Tool()
+        {
+            this.AcquireDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public DateTime AcquireDate { get; set; }
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+}
