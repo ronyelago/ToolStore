a45d31115ea15bc2671d908186b573dd66feaefa
Ronye <ronye.rocha@gmail.com>
2017-08-14 11:45:00 -0300

    PostTool

--- ToolStore.Api/Controllers/ToolController.cs
+++ ToolStore.Api/Controllers/ToolController.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ToolStore.Data.DataContexts;
+using ToolStore.Domain;
 
 namespace ToolStore.Api.Controllers
 {
@@ -13,20 +14,53 @@ namespace ToolStore.Api.Controllers
     {
         private ToolStoreDataContext db = new ToolStoreDataContext();
 
-        [Route("products")]
-        public HttpResponseMessage GetProducts()
+        [Route("tools")]
+        public HttpResponseMessage GetTools()
         {
             var result = db.Tools.Include("Category").ToList();
 
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
 
-        [Route("categories/{categoryId}/products")]
-        public HttpResponseMessage GetToolsByCategories(int categoryId)
+        [Route("categories")]
+        public HttpResponseMessage GetCategories()
         {
-            var result = db.Tools.Include("category").Where(x => x.CategoryId == categoryId).ToList();
+            var result = db.Categories.ToList();
 
             return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("categories/{categoryId}/tools")]
+        public HttpResponseMessage GetToolsByCategories(int categoryId)
+        {
+            var result = db.Tools.Include("Category").Where(x => x.CategoryId == categoryId).ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [HttpPost]
+        [Route("tool")]
+        public HttpResponseMessage PostTool(Tool tool)
+        {
+            if (tool == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                db.Tools.Add(tool);
+                db.SaveChanges();
+
+                var result = tool;
+
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (System.Exception)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao incluir nova Ferramenta");
+            }
         }
 
         protected override void Dispose(bool disposing)
