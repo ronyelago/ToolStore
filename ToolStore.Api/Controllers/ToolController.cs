968fac9b0d832cf2b437c7b2cbd20a55dd7b96b2
Ronye <ronye.rocha@gmail.com>
2017-08-15 14:07:22 -0300

    put, patch, delete

--- ToolStore.Api/Controllers/ToolController.cs
+++ ToolStore.Api/Controllers/ToolController.cs
@@ -22,6 +22,14 @@ namespace ToolStore.Api.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
 
+        [Route("tool/{id}")]
+        public HttpResponseMessage GetTool(int ToolId)
+        {
+            var result = db.Tools.Select(x => x.Id == ToolId);
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
         [Route("categories")]
         public HttpResponseMessage GetCategories()
         {
@@ -60,6 +68,74 @@ namespace ToolStore.Api.Controllers
             {
 
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao incluir nova Ferramenta");
+            }
+        }
+
+        [HttpPatch]
+        [Route("tools")]
+        public HttpResponseMessage PatchTool(Tool tool)
+        {
+            if (tool == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                db.Entry<Tool>(tool).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                Tool t = tool;
+                return Request.CreateResponse(HttpStatusCode.OK, t);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar registro.");
+            }
+        }
+
+        [HttpPut]
+        [Route("tools")]
+        public HttpResponseMessage PutTool(Tool tool)
+        {
+            if (tool == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                db.Entry<Tool>(tool).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                Tool t = tool;
+                return Request.CreateResponse(HttpStatusCode.OK, t);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao alterar registro.");
+            }
+        }
+
+        [HttpDelete]
+        [Route("tools")]
+        public HttpResponseMessage DeleteTool(int ToolId)
+        {
+            if (ToolId <= 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                db.Tools.Remove(db.Tools.Find(ToolId));
+                db.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Registro excluído.");
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao excluir registro.");
             }
         }
 
